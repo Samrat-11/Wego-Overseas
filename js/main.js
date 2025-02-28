f5c576fb0eb0f091aba7b0dffce137383f1c26a0
Samrat-11 <kolaparthisamrat11@gmail.com>
2025-03-01 00:19:41 +0530

    navbar fixed

--- js/main.js
+++ js/main.js
@@ -94,7 +94,7 @@
     }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({scrollTop: 0}, 1000, 'easeInOutExpo');
         return false;
     });
 
