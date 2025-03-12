a0e707a3c451b098092b7f7271e70dd848e101c9
Samrat-11 <kolaparthisamrat11@gmail.com>
2025-03-12 13:41:53 +0530

    testimonials changed

--- js/main.js
+++ js/main.js
@@ -119,10 +119,10 @@ contactMeForm.addEventListener('submit', event => {
     messageText += ` My phone number is ${phone}.`;
   }
   if (project) {
-    messageText += ` I would like to discuss with you regarding a visa and its details are as follows:\n ${project}.`;
+    messageText += ` I would like to discuss visa options with you, as I'm interested in travelling to \n ${project}.`;
   }
   if (subject) {
-    messageText += `\n Subject: ${subject}.`;
+    messageText += `\n for the purpose of ${subject}.`;
   }
   if (message) {
     messageText += `\n Also, please find my comments as follows:\n ${message}`;
